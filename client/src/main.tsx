afff327c09fb0f0af08a6a330264186bb740f3f5
AcePeQ <maciej.nojszewski22@gmail.com>
2026-09-09 17:24:43 +0200

    feat: add reset and fonts CSS files for consistent styling across the application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "./assets/styles/reset.css"
+import "./assets/styles/fonts.css"
+import "./assets/styles/index.css"
 import App from './app/App'
 
 
