fef432696eed0bfbd68cf0e1efc90a2058fd704f
AcePeQ <maciej.nojszewski22@gmail.com>
2026-09-16 14:45:02 +0200

    feat: restructure NotFound component layout and add button styles

--- client/src/app/routes/notFound/NotFound.tsx
+++ client/src/app/routes/notFound/NotFound.tsx
@@ -8,12 +8,14 @@ function NotFound() {
   return (
     <section className={styles.wrapper}>
       <div className={styles.left}>
-        <h1>Page not found</h1>
-        <p>Sorry, the page you are looking for doesn't exist or has been moved. Here are some helpful links:</p>
+        <div className={styles.leftWrapper}>
+          <h1>Page not found</h1>
+          <p>Sorry, the page you are looking for doesn't exist or has been moved. Here are some helpful links:</p>
 
-        <div className={styles.btnWrapper}>
-          <Button type='ghost' onClick={() => { }}>Go back</Button>
-          <ButtonLink type='primary' path='/dashboard'>Take me home</ButtonLink>
+          <div className={styles.btnWrapper}>
+            <Button type='ghost' onClick={() => { }}>Go back</Button>
+            <ButtonLink type='primary' path='/dashboard'>Take me home</ButtonLink>
+          </div>
         </div>
       </div>
       <div className={styles.right}>
