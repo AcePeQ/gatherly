045655d2e18b97a1ebf830c834039e51d9ad5b6e
AcePeQ <maciej.nojszewski22@gmail.com>
2026-09-17 14:36:31 +0200

    feat: implement Auth component with animated background and styling

--- client/src/app/routes/auth/Auth.tsx
+++ client/src/app/routes/auth/Auth.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { useState } from 'react';
+import { motion } from "motion/react"
+
+import styles from './Auth.module.css';
+
+const menuBgVariants = {
+  initial: (isLoginMode: boolean) => ({
+    x: isLoginMode ? "100%" : "0%"
+  }),
+  animate: (isLoginMode: boolean) => ({
+    x: isLoginMode ? "100%" : "0%"
+  })
+}
 
 function Auth() {
+  const [authMode, setAuthMode] = useState<"login" | "register">("login")
+
+  function changeAuthMode(mode: "login" | "register") {
+    setAuthMode(mode);
+  }
+
+  const isLoginMode = authMode === "login";
   return (
-    <div>Auth</div>
+    <section className={styles.wrapper}>
+      <div className={styles.authWrapper}>
+        <div>LOGO</div>
+
+        <hgroup className={styles.titleGroup}>
+          <h1 className={styles.title}>{isLoginMode ? "Log in to your account" : "Create and account"}</h1>
+          <p className={styles.subTitle}>{isLoginMode ? "Welcome back! Please enter your details." : "Start your free planning journey!"}</p>
+        </hgroup>
+
+        <menu className={styles.menu}>
+          <ul className={styles.menuList}>
+            <li className={styles.menuItem}>
+              <button onClick={() => changeAuthMode("register")} className={styles.menuButton}>Sign up</button>
+            </li>
+            <li className={styles.menuItem}>
+              <button onClick={() => changeAuthMode("login")} className={styles.menuButton}>Log in</button>
+            </li>
+
+            <motion.div custom={isLoginMode} variants={menuBgVariants} initial="initial" animate="animate" className={styles.menuActiveBg} aria-hidden="true" />
+          </ul>
+        </menu>
+      </div>
+    </section>
   )
 }
 
